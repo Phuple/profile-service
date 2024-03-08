797a5184037105a094be4f4b0be025c430a7e158
Phuple <135504087+Phuple@users.noreply.github.com>
2024-03-08 20:30:25 +0200

    Initial Commit

---
+++ src/main/kotlin/BaseProfile.kt
@@ -0,0 +1,5 @@
+import java.util.UUID
+
+interface BaseProfile {
+    val id: UUID
+}
---
+++ src/main/kotlin/ProfileService.kt
@@ -0,0 +1,6 @@
+import java.util.HashMap
+import java.util.UUID
+
+object ProfileService {
+    val profiles: HashMap<UUID, out BaseProfile> = HashMap()
+}
